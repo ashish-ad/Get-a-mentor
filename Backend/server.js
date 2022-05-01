7a24d9180452dccf7bc4e3c0afe2a4ce8060a6eb
Ashish Pradhan <73051572+ashish-ad@users.noreply.github.com>
2022-05-02 01:19:26 +0530

    File structure change and some changes in backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,21 @@ import mongooseConnection from "./DB/index.js";
 import router from "./Routes/route.js";
 import dotnet from 'dotenv';
 
+<<<<<<< HEAD
 const app = express();
 app.use(express.json());
 
+=======
+const app = express(); 
 
+dotnet.config();
 
+const port = process.env.PORT || 5000;
+>>>>>>> e0191f3 (File structure change and some changes in backend)
+
+app.use("/", router);
+
+<<<<<<< HEAD
 const port = process.env.PORT || 5000;
 
 app.use("/", router);
@@ -16,6 +26,12 @@ app.all("*", (req, res) => {
   res.send("Invalid Request. Please contact Adminitstrator.");
 });
 
+=======
+app.all("*", (req, res) => {
+  res.send("Invalid Request. Please contact Adminitstrator.");
+});
+
+>>>>>>> e0191f3 (File structure change and some changes in backend)
 const initializeServer = async () => {
   await mongooseConnection();
   app.listen(port, (err) => {
@@ -26,4 +42,8 @@ const initializeServer = async () => {
   });
 };
 
+<<<<<<< HEAD
 initializeServer();
+=======
+initializeServer();  
+>>>>>>> e0191f3 (File structure change and some changes in backend)
